9875639a9c5dc6f603a69aff9ec19410ff184cae
Mariana Dias Almeida <99661953+MarianaDiasA@users.noreply.github.com>
2025-09-16 20:34:47 -0300

    Update Tarefa.java
    - Adicionei o dataHora do tipo LocalDateTime;
    - Inclui o campo dataHora nos construtores existentes.
    - Criei novos construtors para sobrecarga.
    - Pequenas alterações para melhoria de código.

--- src/main/java/modelo/Tarefa.java
+++ src/main/java/modelo/Tarefa.java
@@ -1,22 +1,37 @@
 package modelo;
 
+import java.time.LocalDateTime;
+
 public class Tarefa {
     private Long id;
     private String titulo;
     private String descricao;
     private boolean completa;
+    private LocalDateTime dataHora;
 
+    public Tarefa(){
+        this.dataHora = LocalDateTime.now();
+        this.completa = false;
+    }
+
+    public Tarefa(String titulo){
+        this();
+        this.titulo = titulo;
+    }
+    
     public Tarefa(Long id, String titulo, String descricao) {
         this.id = id;
         this.titulo = titulo;
         this.descricao = descricao;
         this.completa = false;
+        this.dataHora = LocalDateTime.now();
     }
 
     public Tarefa(String titulo, String descricao) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.completa = false;
+        this.dataHora = LocalDateTime.now();
     }
 
     public Long getId() { return id; }
@@ -28,6 +43,10 @@ public class Tarefa {
 
     public boolean isCompleta() { return completa; }
     public void setCompleta(boolean completa) { this.completa = completa; }
+
+    public LocalDateTime getDataHora(){
+        return this.dataHora;
+    }
 
     @Override
     public String toString() {
